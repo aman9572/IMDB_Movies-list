bce4101c5e29fd350748a395b8f523a43fa6bec2
aman9572 <amansinghrajput9572@gmail.com>
2022-10-07 18:49:04 +0530

    commiting

--- IMDB-clone-ReactApp-master/src/App.js
+++ IMDB-clone-ReactApp-master/src/App.js
@@ -8,15 +8,15 @@ import Movie from './pages/movieDetail/movie';
 function App() {
   return (
     <div className="App">
-        <Router>
-          <Header />
-            <Routes>
-                <Route index element={<Home />}></Route>
-                <Route path="movie/:id" element={<Movie />}></Route>
-                <Route path="movies/:type" element={<MovieList />}></Route>
-                <Route path="/*" element={<h1>Error Page</h1>}></Route>
-            </Routes>
-        </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path="/*" element={<h1>Error Page</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
